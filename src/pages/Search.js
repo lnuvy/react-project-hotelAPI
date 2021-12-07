5bec44214f5e17d9df2b649d6b0cdd82879ad163
lnuvy <hangeulgod@naver.com>
2021-12-07 14:13:09 +0900

    사용자입력 콘솔로그로 전달받기

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "../components";
 import './CSS/Search.css'
 
 const Search = () => {
+
+  const [destination, setDestination] = useState('')
+  const [checkIn, setCheckIn] = useState('')
+  const [checkOut, setCheckOut] = useState('')
+  const [adultsNumber, setAdultsNumber] = useState(1)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    console.log(name, value);
+
+    switch(name) {
+      case 'destination' :
+        setDestination(value)
+        break;
+      case 'check-in' :
+        setCheckIn(value)
+        break;
+      case 'check-out' :
+        setCheckOut(value)
+        break;
+      case 'adults-number' :
+        setAdultsNumber(value)
+        break;
+      
+    }
+  }
+
   return (
     <div className="Search-container">
       <div className="Search-inputs">
-        <Input name="destination" type="text" placeholder="목적지를 입력하시오" width="middle" />
-        <Input name="check-in" type="date" placeholder="체크인" width="small" />
-        <Input name="check-out" type="date" placeholder="체크아웃" width="small" />
-        <Input name="adults-number" type="number" placeholder="인원수" width="small" min={1} max={7} />
+        <Input name="destination" type="text" placeholder="목적지를 입력하시오" width="middle"
+          value={destination} onChange={handleChange} />
+        <Input name="check-in" type="date" placeholder="체크인" width="small"
+          value={checkIn} onChange={handleChange} />
+        <Input name="check-out" type="date" placeholder="체크아웃" width="small"
+          value={checkOut} onChange={handleChange} />
+        <Input name="adults-number" type="number" placeholder="인원수" width="small"
+          value={adultsNumber} onChange={handleChange} min={1} max={7} />
       </div>
     </div>
   )
