38623ce6b6f8832db9cfe680bdcf105bde454e19
lnuvy <hangeulgod@naver.com>
2021-12-07 13:44:37 +0900

    import BrowserRouter 로 Hello React 확인

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
     return (
@@ -7,4 +8,6 @@ const App = () => {
     );
   };
   
-  ReactDOM.render(<App />, document.getElementById("app"));
+  ReactDOM.render(<BrowserRouter>
+                    <App />
+                  </BrowserRouter>, document.getElementById("app"));
