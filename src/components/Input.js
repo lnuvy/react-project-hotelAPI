5efae566e210bfe38c43ccdf25e7931b1c096c67
lnuvy <hangeulgod@naver.com>
2021-12-07 13:55:52 +0900

    Input 컴포넌트 정의

---
+++ src/components/Input.js
@@ -0,0 +1,17 @@
+
+const Input = ({ ...rest }) => {
+  const { name, type, placeholder, value, onChange, width, min, max } = rest;
+
+  return (
+    <input className={`Input-container ${width}`} name={name} type={type} min={min}
+      max={max} placeholder={placeholder} value={value} onChange={onChange} />
+  )
+}
+
+export default Input
+
+Input.defaultProps = {
+  width: 'middle',
+  min: 0,
+  max: 0,
+}
