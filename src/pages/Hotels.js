050192f97f50222d3def7bd8f0d67232e82bdcb4
lnuvy <hangeulgod@naver.com>
2021-12-07 16:14:52 +0900

    Hotels 페이지 useEffect 추가 (Location Hook 통해 data 전달 확인)

--- src/pages/Hotels.js
+++ src/pages/Hotels.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 import { fetchHotelsCom, isArrayNull } from 'lib'
@@ -9,7 +9,13 @@ import './CSS/Hotels.css'
 const Hotels = () => {
   const location = useLocation()
   const { destinationId, checkIn, checkOut, adultsNumber } = location
-  console.log(destinationId, checkIn, checkOut, adultsNumber);
+
+  const [hotels, setHotels] = useState([])
+
+  useEffect( async () => {
+    const hotelsList = await getHotels()
+    setHotels(hotelsList)
+  }, [])
 
   const getHotels = async () => {
     // const data = await fetchHotelsCom(`https://hotels-com-provider.p.rapidapi.com/v1/hotels/search?checkin_date=${checkIn}&checkout_date=${checkOut}&sort_order=STAR_RATING_HIGHEST_FIRST&destination_id=${destinationId}&adults_number=${adultsNumber}&locale=ko_KR&currency=KRW`)
